1fda4d48945a4742f9f6eed1d00705b86c7d1f54
antunesnick <nickolasalmeida2@gmail.com>
2024-04-15 19:31:10 -0300

    Catch existing user exception

--- src/main/java/com/lucasangelo/todosimple/services/UserService.java
+++ src/main/java/com/lucasangelo/todosimple/services/UserService.java
@@ -1,6 +1,7 @@
 package com.lucasangelo.todosimple.services;
 import java.util.Optional;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 import com.lucasangelo.todosimple.models.User;
@@ -15,16 +16,21 @@ public class UserService {
 
 
     public User findById(Long id) {
-        Optional<User> user = this.userRepository.findById(id);
+        Optional<User> user = this.userRepository.findById(id); 
 
         return user.orElseThrow(() -> new RuntimeException("Usuário não encontrado. Id:" + id + ", Tipo: " +  User.class.getName()));
     }
 
     @Transactional
     public User create(User obj) {
-        obj.setId(null);
-        obj = this.userRepository.save(obj);
-        return obj;
+        try{
+            obj.setId(null);
+            this.userRepository.save(obj);
+            return obj;
+        }
+        catch(DataIntegrityViolationException dataIntegrityViolationException){
+            throw new RuntimeException("ERRO usúario já existente.");
+        }
     }
 
     @Transactional
